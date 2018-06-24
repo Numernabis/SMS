62f091afc7020dfb53d5d4db700b2bf860f623c8
Wojtek Wanczyk <wojtekwanczyk@gmail.com>
2018-06-24 11:20:34 +0200

    added three levels (requires optimization)

--- src/main/scala/BoardGUI.scala
+++ src/main/scala/BoardGUI.scala
@@ -6,8 +6,7 @@ import scalafx.application.JFXApp
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
 import scalafx.scene.paint.Color._
-import scalafx.scene.control.{Button, TextField}
-import scalafx.scene.effect.DropShadow
+import scalafx.scene.control.{Button, RadioButton, ToggleGroup}
 import scalafx.scene.image.{Image, ImageView}
 import scalafx.scene.layout.HBox
 import scalafx.scene.paint.{LinearGradient, Stops}
@@ -15,152 +14,232 @@ import scalafx.scene.text.Text
 
 object BoardGUI extends JFXApp {
 
-    val imgNormal = "images/tile.png"
-    val imgPressed = "images/tile_pressed.png"
-    val imgBomb = "images/bomb.png"
-    val imgFlag = "images/flag.png"
-    val imgHint = new Array[String](9)
-    for (i <- 1 until 9) {
-        imgHint(i) = s"images/$i.png"
-    }
-  var gameRunning = true
+  val imgNormal = "images/tile.png"
+  val imgPressed = "images/tile_pressed.png"
+  val imgBomb = "images/bomb.png"
+  val imgFlag = "images/flag.png"
+  val imgHint = new Array[String](9)
+  for (i <- 1 until 9) {
+    imgHint(i) = s"images/$i.png"
+  }
+  var gameRunning = false
+
+  val tileWidth = 40
+  val tileHeight = 40
+
+  var tilesX = 10 //TODO: jako zmienne, w zależności od poziomu
+  var tilesY = 10 //TODO: jw.
+  var bombNr = 9 //TODO: jw.
 
 
+  var logicBoard = new Game(tilesX, tilesY, bombNr)
+  var gameScene: Scene = makeScene(tilesX, tilesY)
 
-    val tileWidth = 40
-    val tileHeight = 40
+  var imgBoard = Array.ofDim[ImageView](tilesY, tilesX)
+  for (j <- 0 until tilesX; i <- 0 until tilesY) {
+    val im = new ImageView(new Image(imgNormal))
+    im.x = j * tileWidth
+    im.y = i * tileHeight
+    im.fitHeight = tileHeight
+    im.fitWidth = tileWidth
+    imgBoard(i)(j) = im
+  }
+  var boardFlat = imgBoard.flatten
 
-    val tilesX = 15 //TODO: jako zmienne, w zależności od poziomu
-    val tilesY = 10 //TODO: jw.
-    val bombNr = 9  //TODO: jw.
 
+  val startText = new HBox {
+    autosize()
+    layoutX = 0
+    layoutY = 0
 
-
-
-
-    var logicBoard = new Game(tilesX, tilesY, bombNr)
-
-    var imgBoard = Array.ofDim[ImageView](tilesY, tilesX)
-    for (j <- 0 until tilesX; i <- 0 until tilesY) {
-        val im = new ImageView(new Image(imgNormal))
-        im.x = j * tileWidth
-        im.y = i * tileHeight
-        im.fitHeight = tileHeight
-        im.fitWidth = tileWidth
-        imgBoard(i)(j) = im
-    }
-    var boardFlat = imgBoard.flatten
-
-    val myScene: Scene = new Scene(tileWidth * tilesX, tileHeight * tilesY) {
-
-      val gameOver = new HBox {
-        autosize()
-        layoutX = tilesX * tileWidth / 2 - 190
-        layoutY = tilesY * tileHeight / 2 - 80
-
-        padding = Insets(20)
-        children = Seq(
-          new Text {
-            text = "Game Over!"
-            style = "-fx-font-size: 48pt"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(Red, DarkRed))
-          }
-        )
+    padding = Insets(20)
+    children = Seq(
+      new Text {
+        text = "Welcome to MineSweeper!"
+        style = "-fx-font-size: 35pt"
+        fill = new LinearGradient(
+          endX = 0,
+          stops = Stops(BlueViolet, DarkRed))
       }
+    )
+  }
 
-        val startButton = new Button("Start!"){
-          layoutX = tilesX * tileWidth / 2 - 30
-          layoutY = tilesY * tileHeight / 2 - 15
-          onMouseClicked = (m: MouseEvent) =>{
-            content = boardFlat
+  val levelText = new HBox {
+    autosize()
+    layoutX = 170
+    layoutY = 100
+
+    padding = Insets(20)
+    children = Seq(
+      new Text {
+        text = "Choose level"
+        style = "-fx-font-size: 25pt"
+        fill = new LinearGradient(
+          endX = 0,
+          stops = Stops(BlueViolet, Black))
+      }
+    )
+  }
+
+
+
+  val level1: RadioButton = new RadioButton("Easy") {
+    layoutX = 250
+    layoutY = 200
+  }
+  val level2: RadioButton = new RadioButton("Medium") {
+    layoutX = 250
+    layoutY = 230
+  }
+
+  val level3: RadioButton = new RadioButton("Hard") {
+    layoutX = 250
+    layoutY = 260
+  }
+  val toggle = new ToggleGroup()
+  toggle.toggles = List(level1, level2, level3)
+
+
+
+
+  val startButton: Button = new Button("Start!") {
+    layoutX = 250
+    layoutY = 300
+
+    onMouseClicked = (m: MouseEvent) => {
+      if(!(toggle.getSelectedToggle == null)){
+
+        if(level1.selected()){
+          tilesX = 10
+          tilesY = 10
+          bombNr = 10
+        } else if(level2.selected()){
+          tilesX = 20
+          tilesY = 15
+          bombNr = 10
+        } else if(level3.selected()){
+          tilesX = 25
+          tilesY = 15
+          bombNr = 10
+        }
+
+
+        println(toggle.getSelectedToggle)
+        gameRunning = true
+        gameScene = makeScene(tilesX, tilesY)
+        logicBoard = new Game(tilesX, tilesY, bombNr)
+        imgBoard = Array.ofDim[ImageView](tilesY, tilesX)
+        refresh()
+        stage.scene = gameScene
+      }
+    }
+  }
+
+
+  val welcomeScreen = List(startButton, level1, level2, level3, startText, levelText)
+
+
+
+  val welcomeScene: Scene = new Scene(600, 400) {
+    content = welcomeScreen
+  }
+
+
+  def makeScene(x: Int, y: Int): Scene ={
+    new Scene(x * tileWidth, y * tileHeight) {
+      onMouseClicked = (m: MouseEvent) => {
+
+        if (gameRunning) {
+          val nr_x = (m.getX / tileWidth).asInstanceOf[Int]
+          val nr_y = (m.getY / tileHeight).asInstanceOf[Int]
+          //println(nr_x, nr_y)
+
+          m.getButton match {
+            case MouseButton.PRIMARY =>
+              logicBoard = logicBoard.openCell(nr_x, nr_y)
+              refresh()
+            case MouseButton.SECONDARY =>
+              logicBoard = logicBoard.putFlag(nr_x, nr_y)
+              refresh()
+            case _ =>
+              println("Unknown pattern with Mouse Button")
           }
+        } else {
+          logicBoard = new Game(tilesX, tilesY, bombNr)
+          stage.scene = welcomeScene
         }
-
-
-      val welcomeScreen = List(startButton)
-
-      content = welcomeScreen
-
-
-        onMouseClicked = (m: MouseEvent) => {
-
-          if(gameRunning) {
-            val nr_x = (m.getX / tileWidth).asInstanceOf[Int]
-            val nr_y = (m.getY / tileHeight).asInstanceOf[Int]
-            //println(nr_x, nr_y)
-
-            m.getButton match {
-              case MouseButton.PRIMARY =>
-                logicBoard = logicBoard.openCell(nr_x, nr_y)
-                refresh()
-              case MouseButton.SECONDARY =>
-                logicBoard = logicBoard.putFlag(nr_x, nr_y)
-                refresh()
-              case MouseButton.MIDDLE =>
-              //czy potrzebujemy środkowego przycisku mychy?
-              //putImg(nr_x, nr_y, imgBomb)
-              //flattenAndReplace()
-              case _ =>
-                println("Unknown pattern with Mouse Button")
-            }
-          } else {
-            logicBoard = new Game(tilesX, tilesY, bombNr)
-            gameRunning = true
-            refresh()
-          }
+      }
+    }
+  }
 
 
 
+
+
+  val gameOver = new HBox {
+    autosize()
+    layoutX = tilesX * tileWidth / 2 - 190
+    layoutY = tilesY * tileHeight / 2 - 80
+
+    padding = Insets(20)
+    children = Seq(
+      new Text {
+        text = "Game Over!"
+        style = "-fx-font-size: 48pt"
+        fill = new LinearGradient(
+          endX = 0,
+          stops = Stops(Red, DarkRed))
+      }
+    )
+  }
+
+
+  def flattenAndReplace(): Unit = {
+    boardFlat = imgBoard.flatten
+    gameScene.content = boardFlat
+  }
+
+  def putImg(x: Int, y: Int, name: String): Unit = {
+    val im = new ImageView(new Image(name))
+    im.x = x * tileWidth
+    im.y = y * tileHeight
+    im.fitHeight = tileHeight
+    im.fitWidth = tileWidth
+    imgBoard(y)(x) = im
+  }
+
+  def refresh(): Unit = {
+
+    for (j <- 0 until tilesX; i <- 0 until tilesY) {
+      logicBoard.getCell(j, i) match {
+        case Bomb(1) => {
+          putImg(j, i, imgBomb)
+          gameRunning = false
         }
+        case Blank(1) => putImg(j, i, imgPressed)
+        case Hint(1, x) => putImg(j, i, imgHint(x))
 
-        def flattenAndReplace(): Unit = {
-            boardFlat = imgBoard.flatten
-            content = boardFlat
+        case Blank(2) | Bomb(2) | Hint(2, _) => {
+          putImg(j, i, imgFlag)
         }
-
-        def putImg(x: Int, y: Int, name: String): Unit = {
-            val im = new ImageView(new Image(name))
-            im.x = x * tileWidth
-            im.y = y * tileHeight
-            im.fitHeight = tileHeight
-            im.fitWidth = tileWidth
-            imgBoard(y)(x) = im
+        case Blank(0) | Bomb(0) | Hint(0, _) => {
+          putImg(j, i, imgNormal)
         }
-
-        def refresh(): Unit = {
-
-            for (j <- 0 until tilesX; i <- 0 until tilesY) {
-                logicBoard.getCell(j, i) match {
-                    case Bomb(1)    => {
-                        putImg(j, i, imgBomb)
-                        gameRunning = false
-                    }
-                    case Blank(1)   => putImg(j, i, imgPressed)
-                    case Hint(1, x) => putImg(j, i, imgHint(x))
-
-                    case Blank(2) | Bomb(2) | Hint(2, _) => {
-                        putImg(j, i, imgFlag)
-                    }
-                    case Blank(0) | Bomb(0) | Hint(0, _) => {
-                        putImg(j, i, imgNormal)
-                    }
-                    case _ =>
-                }
-            }
-
-            //TODO: sprawdzenie czy wygrano
-
-            flattenAndReplace()
-
-          if(!gameRunning)
-            content += gameOver
-        }
+        case _ =>
+      }
     }
 
-    stage = new JFXApp.PrimaryStage {
-        scene = myScene
-        title = "Mine Sweeper"
-    }
+    //TODO: sprawdzenie czy wygrano
+
+    flattenAndReplace()
+
+    if (!gameRunning)
+      gameScene.content += gameOver
+  }
+
+
+  stage = new JFXApp.PrimaryStage {
+    scene = welcomeScene
+    title = "Mine Sweeper"
+  }
 }
